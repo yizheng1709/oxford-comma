a358cb5c06517b49b77805a46607cf0e64c52f22
Eva <yi.zheng1709@gmail.com>
2021-01-12 17:58:03 -0500

    commit tree 38181b66f44344568319670430bcd226aec62893

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+    length_of_array = array.length 
+    case length_of_array
+    when 1 
+        array[0]
+    when 2 
+        array.join(" and ")
+    when 3
+        last_item = array.pop
+        array.join(", ") + ", and " + last_item 
+    else 
+        last_item = array.pop 
+        array.join(", ") + ", and " + last_item 
+    end 
 end
